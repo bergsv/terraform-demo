deb4bd991b3aa0a5952a9ec008d49aca9050d86b
Sven Bergendahl <sven.bergendahl@outlook.de>
2023-09-13 09:37:14 +0200

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "azurerm" {
+        version = "~>2.0"
+
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-terraform-demo"
+  location = "westus2"
+}
+
+/* resource "azurerm_virtual_network" "example" {
+  name                = "vnet-terraform-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "subnet-terraform-demo"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "nic-terraform-demo"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "vm-terraform-demo"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  size = "Standard_B1s"
+
+  admin_username = "adminuser"
+  admin_password = "AdminPassword12345!"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+} */
