ba36d32aa185abbd6830beb18a74daea353b07bc
Sven Bergendahl <sven.bergendahl@outlook.de>
2023-09-13 09:43:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,26 @@
-provider "azurerm" {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
 
-  features {
-    
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
 
 resource "azurerm_resource_group" "example" {
   name     = "rg-terraform-demo"
