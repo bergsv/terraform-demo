b3aeb01626f3c81685cf6d1febbe1a6a3ba74752
Sven Bergendahl <sven.bergendahl@outlook.de>
2023-09-15 13:25:02 +0200

    yes

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstate17896"
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstate52343"
     container_name       = "tfstate"
-    key                  = "state.terraform.tfstate"
+    key                  = "foermchen.terraform.tfstate"
     use_oidc             = true
     subscription_id      = "dbbf7928-cbf2-4ffd-80d1-3c35b6cce5aa"
     tenant_id            = "0e3d417e-c425-4353-92d0-406535d8b62b"
