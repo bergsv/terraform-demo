4310101a69b9d459d27f36ef33acc6e967b32c7b
Sven Bergendahl <58976059+bergsv@users.noreply.github.com>
2023-09-15 10:55:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name  = "tfstate"
     storage_account_name = "tfstate17896"
     container_name       = "tfstate"
-    key                  = "IOMwUlqvogo8f2UJag8iUpT6Arg6SFL/JVSea/+0Lyl+LPkPb/+krvZG8FJglmEURhbqlHzhtOEt+ASttyQU6g=="
+    key                  = "prod.terraform.tfstate"
     use_oidc             = true
     subscription_id      = "dbbf7928-cbf2-4ffd-80d1-3c35b6cce5aa"
     tenant_id            = "0e3d417e-c425-4353-92d0-406535d8b62b"
