ee8a837e30cc84846ed662e230df0a4606d3a40e
Sven Bergendahl <sven.bergendahl@outlook.de>
2023-09-15 13:31:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.72.0"
+      version = "~> 3.73.0"
     }
   }
   backend "azurerm" {
@@ -22,6 +22,7 @@ provider "azurerm" {
   features {}
 }
 
+/* 
 resource "azurerm_resource_group" "database" {
   name     = "rg-${var.environment}-database"
   location = "westeurope"
@@ -31,55 +32,4 @@ resource "azurerm_resource_group" "webapp" {
   name     = "rg-${var.environment}-webapp"
   location = "westeurope"
 }
-
-/* resource "azurerm_virtual_network" "example" {
-  name                = "vnet-terraform-demo"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "subnet-terraform-demo"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "nic-terraform-demo"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "vm-terraform-demo"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  size = "Standard_B1s"
-
-  admin_username = "adminuser"
-  admin_password = "AdminPassword12345!"
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-} */
+ */
