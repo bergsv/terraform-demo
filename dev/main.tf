068472bdd22053c1872d7dfb7ac74ddc9a6788cb
Sven Bergendahl <sven.bergendahl@outlook.de>
2023-09-18 14:49:59 +0200

    yes

--- dev/main.tf
+++ dev/main.tf
@@ -11,6 +11,8 @@ terraform {
     storage_account_name = "tfstate52343"
     container_name       = "tfstate"
     use_oidc             = true
+       key                   = "dev.tfstate"
+
     subscription_id      = "dbbf7928-cbf2-4ffd-80d1-3c35b6cce5aa"
     tenant_id            = "0e3d417e-c425-4353-92d0-406535d8b62b"
   }
